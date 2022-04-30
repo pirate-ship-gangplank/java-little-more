61eb46fd4e726efe02323d56f3645d96eb26509a
Diger-king <peter6081@gmail.com>
2022-04-30 21:41:24 +0900

    Study : Step.13 까지 220430

--- src/main/java/digerking/FunctionalProgramming/FunctionalProgrammingNumber.java
+++ src/main/java/digerking/FunctionalProgramming/FunctionalProgrammingNumber.java
@@ -1,6 +1,8 @@
 package digerking.FunctionalProgramming;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FunctionalProgrammingNumber {
@@ -48,6 +50,26 @@ public class FunctionalProgrammingNumber {
         System.out.println("-----------");
 
         Ex3();
+
+        System.out.println("-----------");
+
+        endMax();
+
+        System.out.println("-----------");
+
+        endMin();
+
+        System.out.println("-----------");
+
+        Ex4();
+
+        System.out.println("-----------");
+
+        Ex5();
+
+        System.out.println("-----------");
+
+        OptionalPractice();
     }
 
     // 2.5 일반적인 리스트의 합을 구하는 방식
@@ -122,4 +144,68 @@ public class FunctionalProgrammingNumber {
                 .map(s -> s.length())
                 .forEach(s -> System.out.println(s));
     }
+
+
+    // 2.10 종단연산 - Max
+    private static void endMax() {
+        //optional 객체로 반환
+        System.out.println(List.of(23, 12, 34, 53)
+                .stream()
+                .max((n1, n2) -> Integer.compare(n1, n2)));
+        
+        //optional 객체에서 꺼내기
+        System.out.println(List.of(23, 12, 34, 53)
+                .stream()
+                .max((n1, n2) -> Integer.compare(n1, n2)).get());
+    }
+
+    // 2.11 종단연산 - Min
+    private static void endMin() {
+        //홀수 중에서 최솟값
+        System.out.println(List.of(23, 12, 34, 53).stream()
+                .filter(element -> element %2 != 0)
+                .min((n1, n2) -> Integer.compare(n1, n2)));
+
+        //홀수 값들을 리스트 형식으로
+        System.out.println(List.of(23, 12, 34, 53).stream()
+                .filter(element -> element %2 != 0)
+                .collect(Collectors.toList()));
+    }
+
+    // 2.11 연습문제 - 짝수를 뽑아서 리스트로 저장
+    private static void Ex4() {
+        System.out.println(List.of(23, 12, 34, 53).stream()
+                .filter(e -> e %2 == 0)
+                .collect(Collectors.toList())
+        );
+    }
+
+    // 2.11 연습문제 - 10개의 정수를 제곱한 값을 원소로 갖는 리스트를 만들기
+    private static void Ex5() {
+        //.boxed() -> Stream 객체로 변환해주는 중간 연산
+        
+        System.out.println(
+                IntStream.range(0, 10)
+                        .map(e -> e * e)
+                        .boxed()
+                        .collect(Collectors.toList())
+        );
+    }
+
+    // 2.12 Optional -> NPE 를 피하고자 사용
+    private static void OptionalPractice() {
+        Optional<Integer> test = List.of(23, 45, 67, 12)
+                .stream()
+                .filter(n -> n%2 == 0)
+                .max((n1, n2) -> Integer.compare(n1, n2));
+
+        if (test.isPresent()) {
+            System.out.println(test.get());
+        }
+        else System.out.println("Empty" + test);
+    }
+
+
+
+
 }
--- src/main/java/digerking/FunctionalProgramming/FunctionoalProgramming.md
+++ src/main/java/digerking/FunctionalProgramming/FunctionoalProgramming.md
@@ -144,4 +144,67 @@
     } 
 
 ---
+# Additional For-loop vs Stream
 
+### For-loop(반복문)을 이용한 배열 접근 후 최댓값 도출
+  
+    int[] a = ints;
+    
+    int e = ints.length;
+    
+    int m = Integer.MIN_VALUE;
+    
+    for(int i=0; i < e; i++)
+    if(a[i] > m) m = a[i];
+
+
+
+### Stream을 이용한 배열 접근 후 최댓값 도출
+
+    int m = Arrays.stream(ints)
+    .reduce(Integer.MIN_VALUE, Math::max);
+
+<br>
+
+> 두 코드를 동작시킨 환경에서의 성능차이는 다음과 같다.
+
+int-array, for-loop : 0.36 ms
+
+int-array, seq. stream: 5.35 ms
+
+
+<br>
+단편적인 예시이긴 하지만 일반 배열이아닌 ArrayList에서도 반복문이 더 우세한 결과를 가지고 있었다.
+
+<br>
+
+https://pamyferret.tistory.com/49
+
+또한 위 블로그에 따르면
+
+For-loop이 더 빠른 이유를 설명해놓았는데
+
+
+## - for문은 단순 인덱스 기반이다. 
+####  - for문은 단순 인덱스 기반으로 도는 반복문으로 메모리 접근이기 때문에 Stream에 비해 빠르고 오버헤드도 없다. 
+####  - stream의 경우는 JVM이 이것저것 처리해줘야하는 것들이 많아 실행 시 느릴 수 밖에 없다.
+
+
+## - for문은 컴파일러가 최적화를 시킨다. 
+####  - stream은 java 8부터 지원한 것이고 for문은 그보다 훨씬 오래전부터 계속 사용해왔다. 
+####  - 그만큼 사용되는 컴파일러는 오래 사용된 for문에 대한 처리가 되어 있어 for문의 경우 미리 최적화를 시킬 수 있지만, 
+####  - stream의 경우 신생(?)인 만큼 정보가 없어 for문과 같은 정교한 최적화를 수행할 수 없다.
+
+
+<br>
+이 두가지 근거(?)가 설득력 있게 다가왔다.
+
+---
+
+# Step 12. Optional
+
+
+
+  
+
+---
